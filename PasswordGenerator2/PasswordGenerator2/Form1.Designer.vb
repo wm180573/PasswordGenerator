0dd9c655bfc2c6cd663263f0fcda5d377b822c08
wm180573 <58892167+wm180573@users.noreply.github.com>
2020-02-04 19:25:02 +0000

    Add files via upload

---
+++ PasswordGenerator2/PasswordGenerator2/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnGenerate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtYearBorn = New System.Windows.Forms.TextBox()
+        Me.txtFavColour = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtStreetName = New System.Windows.Forms.TextBox()
+        Me.txtShoeSize = New System.Windows.Forms.TextBox()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnGenerate
+        '
+        Me.BtnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnGenerate.Location = New System.Drawing.Point(235, 308)
+        Me.BtnGenerate.Name = "BtnGenerate"
+        Me.BtnGenerate.Size = New System.Drawing.Size(147, 42)
+        Me.BtnGenerate.TabIndex = 0
+        Me.BtnGenerate.Text = "Generate"
+        Me.BtnGenerate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(89, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Firstname:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(89, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Surname:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(89, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Year born:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(89, 195)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(126, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Favourite colour:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(89, 229)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(118, 20)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Name of street:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(89, 261)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 20)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Shoe Size:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(67, 365)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(162, 20)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Generated password:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(166, 49)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(274, 20)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Please fill out the required information"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(198, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(208, 25)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Password Generator"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(222, 93)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(184, 20)
+        Me.txtFirstName.TabIndex = 10
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(222, 130)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(184, 20)
+        Me.txtSurname.TabIndex = 11
+        '
+        'txtYearBorn
+        '
+        Me.txtYearBorn.Location = New System.Drawing.Point(222, 163)
+        Me.txtYearBorn.Name = "txtYearBorn"
+        Me.txtYearBorn.Size = New System.Drawing.Size(184, 20)
+        Me.txtYearBorn.TabIndex = 12
+        '
+        'txtFavColour
+        '
+        Me.txtFavColour.Location = New System.Drawing.Point(222, 199)
+        Me.txtFavColour.Name = "txtFavColour"
+        Me.txtFavColour.Size = New System.Drawing.Size(184, 20)
+        Me.txtFavColour.TabIndex = 13
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(235, 367)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(184, 20)
+        Me.txtPassword.TabIndex = 14
+        '
+        'txtStreetName
+        '
+        Me.txtStreetName.Location = New System.Drawing.Point(222, 230)
+        Me.txtStreetName.Name = "txtStreetName"
+        Me.txtStreetName.Size = New System.Drawing.Size(184, 20)
+        Me.txtStreetName.TabIndex = 14
+        '
+        'txtShoeSize
+        '
+        Me.txtShoeSize.Location = New System.Drawing.Point(222, 263)
+        Me.txtShoeSize.Name = "txtShoeSize"
+        Me.txtShoeSize.Size = New System.Drawing.Size(184, 20)
+        Me.txtShoeSize.TabIndex = 15
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnClear.Location = New System.Drawing.Point(245, 396)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(147, 42)
+        Me.BtnClear.TabIndex = 16
+        Me.BtnClear.Text = "Clear"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(654, 450)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.txtShoeSize)
+        Me.Controls.Add(Me.txtStreetName)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtFavColour)
+        Me.Controls.Add(Me.txtYearBorn)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnGenerate As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtYearBorn As TextBox
+    Friend WithEvents txtFavColour As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtStreetName As TextBox
+    Friend WithEvents txtShoeSize As TextBox
+    Friend WithEvents BtnClear As Button
+End Class
---
+++ PasswordGenerator2/PasswordGenerator2/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub BtnGenerate_Click(sender As Object, e As EventArgs) Handles BtnGenerate.Click
+        If txtFirstName.Text = "" Or txtSurname.Text = "" Or txtYearBorn.Text = "" Or txtStreetName.Text = "" Or txtShoeSize.Text = "" Then
+            MessageBox.Show("Error, you have left one or more fields blank, please ensure that all fields are filled", "Error while compiling values")
+        Else
+            txtPassword.Text = txtFirstName.Text.Substring(0, 1) + Mid(txtSurname.Text, 2, 1) + Mid(txtYearBorn.Text, 3, 4) + Mid(txtFavColour.Text, 2, 3) + txtStreetName.Text.Substring(0, 3) + txtShoeSize.Text + "VB"
+            MessageBox.Show("Password generated", "Values compiled successfully")
+        End If
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        txtPassword.Text = ""
+        txtFirstName.Text = ""
+        txtSurname.Text = ""
+        txtYearBorn.Text = ""
+        txtFavColour.Text = ""
+        txtStreetName.Text = ""
+        txtShoeSize.Text = ""
+    End Sub
+End Class
